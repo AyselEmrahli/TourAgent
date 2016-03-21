b97baae49d0fce74554bf38d151334646f7da973
Orkhan Abbasli <orkhan.a@code.edu.az>
2016-03-22 03:07:40 +0400

    Added sections toursList and header

--- tours.php
+++ tours.php
@@ -24,11 +24,12 @@
   <body>
     <?php 
       include 'sections/header.php';
+      include 'sections/tourList.php';
     ?>
   	
   	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
   </html>
