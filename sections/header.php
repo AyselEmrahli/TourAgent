42d6895517e9c924d36c04bb135d5b3f21b4afca
Orkhan Abbasli <orkhan.a@code.edu.az>
2016-03-21 23:12:20 +0400

    Created header section

---
+++ sections/header.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="css/custom/header.css">
+
+<div class="container-fluid" id="header-nav">
+  <div class="row">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="media/logo-in.png" alt=""></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class=""><a href="#">Turlar <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Agentliklər</a></li>
+            <li><a href="#">Otellər</a></li>
+            <li><a href="#">Rent a Car</a></li>
+            <li><a href="#">Görməli Yerlər</a></li>
+            <li><a href="#">Xəbərlər</a></li>
+          </ul>
+          <div class="nav navbar-right">
+            <a href="#" class="btn btn-sign-up">Qeydiyyat</a>
+            <a href="#" class="btn btn-sign-in">Giriş</a>
+          </div>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </div>
+</div>
