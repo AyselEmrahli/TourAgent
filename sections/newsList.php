2cb309f5fedc0ac5704c9baf142b1f2e1ba8dbd7
AyselEmrahli <aysel.e@code.edu.az>
2016-03-27 23:24:34 +0500

    changed somethings

--- sections/newsList.php
+++ sections/newsList.php
@@ -2,13 +2,14 @@
 <div id="news-wrap">
 	<div class="container">
 		<!-- News list code here -->
+
+		<!-- Left Section code start -->
 		<div class="col-md-3">
-			
+			<div class="col-md-11">
 			<div class="row">
 				<h3>Kateqoriyalar</h3>
 					<div class="list-group">
-					
-  						<a href="#" class="list-group-item">Azərbaycan</a>
+						<a href="#" class="list-group-item">Azərbaycan</a>
   						<a href="#" class="list-group-item">Dünya</a>
   						<a href="#" class="list-group-item">Mədəniyyət</a>
   						<a href="#" class="list-group-item">Bilirsinizmi?</a>
@@ -25,18 +26,20 @@
   						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
   						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
 					</div>
-			</div>
-			
+				</div>
+		</div>	
 		</div>
-		<div class="col-md-8">
+		<!-- Left Section code end-->
+
+
+		<!-- Right Section code start -->
+	<div class="col-md-8">
+	<div class="jumbotron">
 		<div class="row">
-		<div class="row">
-			<div class="jumbotron">
- 				<img src="media/tour3.jpg">
-			</div>
+			<div class="row">
+				<img src="media/tour3.jpg">
 			</div>
 			<div class="row">
-			<div class="jumbotron ">
 				<hr>
  				<i class="fa fa-calendar"></i>
  				<a href="#">12 Mart 2016</a>
@@ -45,34 +48,32 @@
  				<a href="#">Dünya Xəbərləri</a>
 				<hr>
 			</div>
-			</div>
 
 			<div class="row">
-				<div class="jumbotron">
-					<h2>Duis autodolor sit amet</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				<h2>Duis autodolor sit amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.</p>
-					<button type="button" class="btn btn-primary">Ətraflı</button>
+					consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat</p>
+				<button type="button" class="btn btn-primary">Ətraflı</button>
+			</div>
+		</div>
 			
 
-			</div>
-			</div>
-			</div>
 
 
 
+			<div class="row">
+				<div class="row">
+				<hr>
+ 				<img src="media/tour3.jpg">
+			
+				</div>
 
 				<div class="row">
-		<div class="row">
-			<div class="jumbotron">
-			<hr>
- 				<img src="media/tour3.jpg">
-			</div>
-			</div>
-			<div class="row">
-			<div class="jumbotron">
 				<hr>
  				<i class="fa fa-calendar"></i>
  				<a href="#">12 Mart 2016</a>
@@ -80,30 +81,24 @@
  				<i class="fa fa-tags"></i>
  				<a href="#">Dünya Xəbərləri</a>
  				<hr>
-			</div>
-			</div>
+				</div>
 
-			<div class="row">
-				<div class="jumbotron">
+				<div class="row">
 					<h2>Duis autodolor sit amet</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.</p>
+					consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat</p>
 					<button type="button" class="btn btn-primary">Ətraflı</button>
-				
-
+				</div>
 			</div>
-
-			</div>
-			</div>
-		</div>
-
-
-
-
-
-
-
+		
 	</div>
+	</div>
+		<!-- Right Section code end -->
+	</div>
+
 </div>
