662b11dbf7d2d2a338d7200c7a4cf2eeae28705e
AyselEmrahli <aysel.e@code.edu.az>
2016-03-27 21:30:32 +0500

    newsList.php

--- sections/newsList.php
+++ sections/newsList.php
@@ -1,5 +1,109 @@
+
 <div id="news-wrap">
 	<div class="container">
 		<!-- News list code here -->
+		<div class="col-md-3">
+			
+			<div class="row">
+				<h3>Kateqoriyalar</h3>
+					<div class="list-group">
+					
+  						<a href="#" class="list-group-item">Azərbaycan</a>
+  						<a href="#" class="list-group-item">Dünya</a>
+  						<a href="#" class="list-group-item">Mədəniyyət</a>
+  						<a href="#" class="list-group-item">Bilirsinizmi?</a>
+  						<a href="#" class="list-group-item">Maraqlıdır</a>
+					</div>
+			</div>
+			<div class="row">
+				<h3>Ən çox oxunanlar</h3>
+				<div class="list-group">
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet-2</a></li>
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+					</div>
+			</div>
+			
+		</div>
+		<div class="col-md-8">
+		<div class="row">
+		<div class="row">
+			<div class="jumbotron">
+ 				<img src="media/tour3.jpg">
+			</div>
+			</div>
+			<div class="row">
+			<div class="jumbotron ">
+				<hr>
+ 				<i class="fa fa-calendar"></i>
+ 				<a href="#">12 Mart 2016</a>
+
+ 				<i class="fa fa-tags"></i>
+ 				<a href="#">Dünya Xəbərləri</a>
+				<hr>
+			</div>
+			</div>
+
+			<div class="row">
+				<div class="jumbotron">
+					<h2>Duis autodolor sit amet</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+					<button type="button" class="btn btn-primary">Ətraflı</button>
+			
+
+			</div>
+			</div>
+			</div>
+
+
+
+
+				<div class="row">
+		<div class="row">
+			<div class="jumbotron">
+			<hr>
+ 				<img src="media/tour3.jpg">
+			</div>
+			</div>
+			<div class="row">
+			<div class="jumbotron">
+				<hr>
+ 				<i class="fa fa-calendar"></i>
+ 				<a href="#">12 Mart 2016</a>
+
+ 				<i class="fa fa-tags"></i>
+ 				<a href="#">Dünya Xəbərləri</a>
+ 				<hr>
+			</div>
+			</div>
+
+			<div class="row">
+				<div class="jumbotron">
+					<h2>Duis autodolor sit amet</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+					<button type="button" class="btn btn-primary">Ətraflı</button>
+				
+
+			</div>
+
+			</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
 	</div>
 </div>
