acf95ba227cb9f5d19ba7a08d5f478bd55a797d4
AyselEmrahli <aysel.e@code.edu.az>
2016-03-31 12:47:52 +0500

    files added

--- sections/newsList.php
+++ sections/newsList.php
@@ -1,50 +1,36 @@
 
 <div id="news-wrap">
 	<div class="container">
-		<!-- News list code here -->
-
 		<!-- Left Side Bar codes start -->
-		<div class="col-md-3 left-side-bar">
-				<h3>Kateqoriyalar</h3>
-					<div class="list-group">
-						<a href="#" class="list-group-item">Azərbaycan</a>
-  						<a href="#" class="list-group-item">Dünya</a>
-  						<a href="#" class="list-group-item">Mədəniyyət</a>
-  						<a href="#" class="list-group-item">Bilirsinizmi?</a>
-  						<a href="#" class="list-group-item">Maraqlıdır</a>
-					</div>
-
-					<hr>
-				<h3>Ən çox oxunanlar</h3>
-				<div class="list-group">
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet-2</a></li>
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
-  						<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
-					</div>
+		<div class="col-md-3 col-sm-3 left-side-bar">
+			<h3>Kateqoriyalar</h3>
+			<div class="list-group">
+				<li class="list-group-item"><a href="#">Azərbaycan</a></li>
+				<li class="list-group-item"><a href="#">Dünya</a></li>
+				<li class="list-group-item"><a href="#">Mədəniyyət</a></li>
+				<li class="list-group-item"><a href="#">Bilirsinizmi?</a></li>
+				<li class="list-group-item"><a href="#">Maraqlıdır</a></li>
+			</div>
+			<hr>
+			<h3>Ən çox oxunanlar</h3>
+			<div class="list-group">
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet-2</a></li>
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+				<li class="list-group-item"><a href="#">Lorem ipsum dolor sit amet</a></li>
+			</div>
 		</div>
 		<!-- Left Side Bar codes end-->
-
-
 		<!-- news List Wrap codes start -->
-	<div class="col-md-9 news-list-wrap">
-	<div class="jumbotron">
-			<div class="row">
-				<img src="media/tour3.jpg"  class="img-responsive">
-			</div>
-			<div class="row">
-				<hr>
- 				<i class="fa fa-calendar"></i>
- 				<span>12 Mart 2016</span>
-
- 				<i class="fa fa-tags"></i>
- 				<span>Dünya Xəbərləri</span>
-				<hr>
-			</div>
-
-			<div class="row">
+		<div class="col-md-9 col-sm-9 news-list-wrap">
+			<div class="news-item">
+				<img src="media/news1.jpg"  class="img-responsive">
+				<div class="news-info">					
+	 				<span><i class="fa fa-calendar"></i> 12 Mart 2016 </span>
+	 				<span><i class="fa fa-tags"></i> Dünya Xəbərləri </span>					
+				</div>
 				<h2>Duis autodolor sit amet</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -52,43 +38,27 @@
 					consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat</p>
-				<button type="button" class="btn btn-primary">Ətraflı</button>
-			</div>
-			
-			
-				<div class="row">
-				<hr>
- 				<img src="media/tour3.jpg">
-			
+					consequat...</p>
+				<a href="#" class="btn btn-primary">Ətraflı</a>
+			</div>	
+			<hr>
+			<div class="news-item">
+				<img src="media/news1.jpg"  class="img-responsive">
+				<div class="news-info">					
+	 				<span><i class="fa fa-calendar"></i> 12 Mart 2016 </span>
+	 				<span><i class="fa fa-tags"></i> Dünya Xəbərləri </span>					
 				</div>
-
-				<div class="row">
-				<hr>
- 				<i class="fa fa-calendar"></i>
- 				<span>12 Mart 2016</span>
-
- 				<i class="fa fa-tags"></i>
- 				<span>Dünya Xəbərləri</span>
- 				<hr>
-				</div>
-
-				<div class="row">
-					<h2>Duis autodolor sit amet</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				<h2>Duis autodolor sit amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 					consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat</p>
-					<button type="button" class="btn btn-primary">Ətraflı</button>
-					</div>
-		
-
-	
-	</div>
+					consequat...</p>
+				<a href="#" class="btn btn-primary">Ətraflı</a>
+			</div>			
+		</div>
 		<!-- news List Wrap codes end -->
 	</div>
-
 </div>
