585153108c76e7532eb4ab1915d699b96844bcf1
Orkhan Abbasli <Orkhan Abbasli>
2016-04-01 17:35:14 +0400

    Added new Single Tour Page

---
+++ sections/singleTour.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>TourAgent | Azerbaijan</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+     <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="../css/custom/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div id="singletour-wrap">
+      <div class="container-fuild" id="top-banner">
+        <img src="../media/paristour.jpg" class="banner-img">
+        <div id="tourinfo">
+          <div class="container">
+             <!-- <div class="row"> -->
+                <div id="tourname" >
+                  <p>PARIS TURU <span id="price"><small>*bir nəfər üçün</small> 230 <i class="fa fa-euro"></i></span></p>
+                  <p class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <small>(3.5)</small>
+                  </p>
+                </div>
+            <!-- </div> -->
+          </div>         
+        </div>
+      </div>
+      <!-- Top banner end here -->
+      <div class="container" id="content-wrap">
+        <div class="col-md-8">
+          <div class="row" style="position: relative">
+            <div class="page-header">
+              <h1>Tur Haqqında</h1>
+            </div>
+            <div class="tour-feat col-md-3 col-sm-3 col-xs-6">
+              <img class="center-block" src="../media/tour-duration.png">
+              <span>Tur müddəti</span>
+              <p>7 gecə / 8 gün</p>
+            </div>
+            <div class="tour-feat col-md-3 col-sm-3 col-xs-6">
+              <img class="center-block" src="../media/lang.png">
+              <span>Dil</span>
+              <p>Azərbaycan / Rus</p>
+            </div>
+            <div class="tour-feat col-md-3 col-sm-3 col-xs-6">
+              <img class="center-block" src="../media/tour-type.png">
+              <span>Turun tipi</span>
+              <p>Wedding turu</p>
+            </div>
+            <div class="tour-feat col-md-3 col-sm-3 col-xs-6">
+              <img class="center-block" src="../media/last-date.png">
+              <span>Son Qeydiyyat tarixi</span>
+              <p>16 Mart, 2016</p>
+            </div>          
+          </div>
+          <div class="row">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <table class="table">
+                  <tbody>
+                    <tr>
+                      <td><b>Ölkə</b></td>
+                      <td>Fransa</td>
+                    </tr>
+                    <tr>
+                      <td><b>Şəhər / Region</b></td>
+                      <td>Paris</td>
+                    </tr>
+                    <tr>
+                      <td><b>Tarix</b></td>
+                      <td>30 Mart, 2016 - 7 Aprel, 2016</td>
+                    </tr>
+                    <tr>
+                      <td><b>Nəqliyyat</b></td>
+                      <td>Təyyarə</td>
+                    </tr>
+                    <tr>
+                      <td><b>Otel / Geceləmə</b></td>
+                      <td>Hotel Saint German</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <a class="btn btn-primary pull-right"><i class="fa fa-sun-o"></i> Hava haqqında</a>
+                <a class="btn btn-primary pull-right"><i class="fa fa-thumb-tack"></i> Turun proqramı</a>
+              </div>
+            </div>
+          </div>
+          <div class="tour-note">
+            <div class="page-header">
+              <h1>Qeydlər</h1>
+            </div>
+            
+            <ul class="list-group col-md-6">
+              <p><i class="fa fa-check"></i> Ödənişə daxildir</p>
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+
+            <ul class="list-group col-md-6">
+              <p><i class="fa fa-exclamation-triangle"></i> Diqqət</p>
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+          </div>          
+        </div>
+        <!-- Reservation section start here -->
+        <div class="col-md-4">
+          <div class="page-header panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Reservasiya</h3>
+            </div>
+            <div class="panel-body">
+              <div class="col-md-12">
+                <label>Böyüklər</label>
+                <div class="col-md-6 pull-right">
+                  <div class="input-group">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">-</button>
+                    </span>
+                    <input type="number" class="form-control" min="1">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">+</button>
+                    </span>
+                  </div><!-- /input-group -->
+                </div><!-- /.col-lg-6 -->
+              </div>
+              <div class="col-md-12">
+                <label>Böyüklər</label>
+                <div class="col-md-6 pull-right">
+                  <div class="input-group">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">-</button>
+                    </span>
+                    <input type="number" class="form-control" min="1">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">+</button>
+                    </span>
+                  </div><!-- /input-group -->
+                </div><!-- /.col-lg-6 -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Reservation section end here -->
+      </div>
+    </div>       
+     
+  	
+  	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+<script type="text/javascript">
+var spins = document.getElementsByClassName("input-group");
+for (var i = 0, len = spins.length; i < len; i++) {
+    var spin = spins[i],
+        span = spin.getElementsByClassName("input-group-addon"),
+        input = spin.getElementsByClassName("form-control")[1];
+
+    input.onchange = function() { input.value = +input.value || 1; };
+    span[0].onclick = function() { input.value = Math.max(1, input.value - 1); };
+    span[1].onclick = function() { input.value -= -1; };
+}
+
+
+</script>
+  </body>
+  </html>
